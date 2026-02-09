23b969d3f769f24d829901085a97c6215c5b16a2
User <User@WIN-BT7OLJV0750>
2026-02-09 13:50:05 +0500

    разработана форма выдач

---
+++ LibraryProject/FormLoans.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace LibraryProject
+{
+    partial class FormLoans
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panelTop = new Panel();
+            btnBack = new Button();
+            lblUserName = new Label();
+            btnLogout = new Button();
+            dgvLoans = new DataGridView();
+            panelTop.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvLoans).BeginInit();
+            SuspendLayout();
+            // 
+            // panelTop
+            // 
+            panelTop.Controls.Add(btnBack);
+            panelTop.Controls.Add(lblUserName);
+            panelTop.Controls.Add(btnLogout);
+            panelTop.Dock = DockStyle.Top;
+            panelTop.Location = new Point(0, 0);
+            panelTop.Name = "panelTop";
+            panelTop.Padding = new Padding(5);
+            panelTop.Size = new Size(984, 40);
+            panelTop.TabIndex = 4;
+            // 
+            // btnBack
+            // 
+            btnBack.BackColor = Color.FromArgb(74, 111, 165);
+            btnBack.Dock = DockStyle.Left;
+            btnBack.FlatAppearance.BorderSize = 0;
+            btnBack.FlatStyle = FlatStyle.Flat;
+            btnBack.ForeColor = Color.White;
+            btnBack.Location = new Point(5, 5);
+            btnBack.Name = "btnBack";
+            btnBack.Size = new Size(150, 30);
+            btnBack.TabIndex = 7;
+            btnBack.Text = "Назад";
+            btnBack.UseVisualStyleBackColor = false;
+            // 
+            // lblUserName
+            // 
+            lblUserName.AutoSize = true;
+            lblUserName.Dock = DockStyle.Right;
+            lblUserName.Location = new Point(784, 5);
+            lblUserName.Name = "lblUserName";
+            lblUserName.Size = new Size(45, 19);
+            lblUserName.TabIndex = 6;
+            lblUserName.Text = "label1";
+            // 
+            // btnLogout
+            // 
+            btnLogout.BackColor = Color.FromArgb(74, 111, 165);
+            btnLogout.Dock = DockStyle.Right;
+            btnLogout.FlatAppearance.BorderSize = 0;
+            btnLogout.FlatStyle = FlatStyle.Flat;
+            btnLogout.ForeColor = Color.White;
+            btnLogout.Location = new Point(829, 5);
+            btnLogout.Name = "btnLogout";
+            btnLogout.Size = new Size(150, 30);
+            btnLogout.TabIndex = 5;
+            btnLogout.Text = "Выйти";
+            btnLogout.UseVisualStyleBackColor = false;
+            // 
+            // dgvLoans
+            // 
+            dgvLoans.AllowUserToAddRows = false;
+            dgvLoans.AllowUserToDeleteRows = false;
+            dgvLoans.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvLoans.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dgvLoans.BackgroundColor = Color.White;
+            dgvLoans.BorderStyle = BorderStyle.None;
+            dgvLoans.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvLoans.ColumnHeadersVisible = false;
+            dgvLoans.Dock = DockStyle.Fill;
+            dgvLoans.Location = new Point(0, 0);
+            dgvLoans.MultiSelect = false;
+            dgvLoans.Name = "dgvLoans";
+            dgvLoans.RowHeadersVisible = false;
+            dgvLoans.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvLoans.Size = new Size(984, 661);
+            dgvLoans.TabIndex = 5;
+            // 
+            // FormLoans
+            // 
+            AutoScaleDimensions = new SizeF(9F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(984, 661);
+            Controls.Add(panelTop);
+            Controls.Add(dgvLoans);
+            Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            Margin = new Padding(4, 4, 4, 4);
+            Name = "FormLoans";
+            Text = "FormLoans";
+            panelTop.ResumeLayout(false);
+            panelTop.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvLoans).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panelTop;
+        private Button btnBack;
+        private Label lblUserName;
+        private Button btnLogout;
+        private DataGridView dgvLoans;
+    }
+}
